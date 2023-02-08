fdddc707c4e8c583a10aded1fda809b9c4aa6b78
Saulo Almeida <saulohenrique786@gmail.com>
2023-02-08 20:59:43 -0300

    feat: up project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (_, res) => res.send("Hello Docker!"))
+
+app.listen(port, () => {
+    console.log('Running on port ' + port)
+})
